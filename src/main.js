2710174e4eb880ed19e4df64e84d7eba00d54456
Alberich LLerenas <allerenas4@ucol.mx>
2020-12-08 14:51:07 -0600

    implemented sumarParesDo

--- src/main.js
+++ src/main.js
@@ -48,7 +48,20 @@ contarImparesWhile(inicio, fin) {
     }
 return impares;
 }
+sumarParesDo() {
+    let i = 1;
+    let suma = 0;
 
+    do {
+
+    if(i%2 == 0) {
+        suma = suma + i;
+    }
+    
+    i++;
+    }while(i <= 20 );
+    return suma;
+}
 }
 
 let app = new App();
@@ -56,3 +69,4 @@ console.log(app.sumarParesFor());
 console.log(app.contarImparesFor(5,11));
 console.log(app.sumarParesWhile());
 console.log(app.contarImparesWhile(5,11));
+console.log(app.sumarParesDo());
